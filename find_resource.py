2324054764fdf4b283118f52f7b4fc44b569ecf2
mobri2a <mobrien@elvantech.com>
2019-09-24 18:01:38 -0400

    Update find_resource.py

--- find_resource.py
+++ find_resource.py
@@ -8,74 +8,83 @@ from aws_helpers import AWSClient, getLambdaEnv, configLogging
 import json
 import operator
 import re
+from botocore.vendored import requests
+from botocore.config import Config
 
 # INIT
 LOGGING_LEVEL = None
 AWS = None
 LOGGER = None
-def get_az_for_subnet(subnetArg):
-	'''
-	Give str(subnetId)
-	Return str(AZName)
-	'''
-	az = None
-	subnets = AWS.get_simple(REGION, 'ec2', 'describe_subnets', 'Subnets')
-	for subnet in subnets:
-		if subnet['SubnetId'] == subnetArg:
-			az = subnet['AvailabilityZone']
-	return az
 
-def get_ami(props):
-	'''
-	Given queryprops, return the newest matching AMI Id
-	'''
-	archToAMINamePattern = {
-		'PV64-EBS': 'amzn-ami-pv*-x86_64-ebs',
-		'HVM64-GP2': 'amzn-ami-hvm*.x86_64-gp2',
-		'HVM64-2-GP2': 'amzn2-ami-hvm-2.0.*-x86_64-gp2',
-		'HVMG2-EBS': 'amzn-ami-graphics-hvm-*x86_64-ebs*',
-	}
+class Finder:
+	def __init__(self):
+		self.AWS = AWSClient()
+		pass
 
-	if 'Architecture' not in props:
-		return {'Error': 'Architecture not in QueryProps'}
-	if props['Architecture'] not in archToAMINamePattern:
-		return {'Error': 'Unrecognized Architecture. Select one of ' + json.dumps(archToAMINamePattern)}
+	def get_az_for_subnet(self, subnetArg):
+		'''
+		Give str(subnetId)
+		Return str(AZName)
+		'''
+		az = None
+		subnets = self.AWS.get_simple(REGION, 'ec2', 'describe_subnets', 'Subnets')
+		for subnet in subnets:
+			if subnet['SubnetId'] == subnetArg:
+				az = subnet['AvailabilityZone']
+		return az
 
-	filters = {
-		"Filters": [
-			{
-				"Name": "name",
-				"Values": [ archToAMINamePattern[props['Architecture']] ]
-			}
-		]
-	}
+	def get_ami(self, props):
+		'''
+		Given queryprops, return the newest matching AMI Id
+		'''
+		archToAMINamePattern = {
+			'PV64-EBS': 'amzn-ami-pv*-x86_64-ebs',
+			'HVM64-GP2': 'amzn-ami-hvm*.x86_64-gp2',
+			'HVM64-2-GP2': 'amzn2-ami-hvm-2.0.*-x86_64-gp2',
+			'HVMG2-EBS': 'amzn-ami-graphics-hvm-*x86_64-ebs*',
+		}
 
-	amiidlist = AWS.get_simple(REGION, 'ec2', 'describe_images', 'Images', filters)
+		if 'Architecture' not in props:
+			return {'Error': 'Architecture not in QueryProps'}
+		if props['Architecture'] not in archToAMINamePattern:
+			return {'Error': 'Unrecognized Architecture. Select one of ' + json.dumps(archToAMINamePattern)}
 
-	amiid = None
-	newest = ''
-	if amiidlist:
-		for ami in amiidlist:
-			# Ignore release candidates
-			if re.match('.*-rc-.*', ami['Name']):
-				LOGGER.debug('Ignore RC ' + ami['Name'])
-				continue
-			if ami['Name'] > newest:
-				newest = ami['Name']
-				LOGGER.debug('newest is now ' + ami['Name'])
-				amiid = ami['ImageId']
-	
-	return amiid
+		filters = {
+			"Filters": [
+				{
+					"Name": "name",
+					"Values": [ archToAMINamePattern[props['Architecture']] ]
+				}
+			]
+		}
+
+		amiidlist = self.AWS.get_simple(REGION, 'ec2', 'describe_images', 'Images', filters)
+
+		amiid = None
+		newest = ''
+		if amiidlist:
+			for ami in amiidlist:
+				# Ignore release candidates
+				if re.match('.*-rc-.*', ami['Name']):
+					LOGGER.debug('Ignore RC ' + ami['Name'])
+					continue
+				if ami['Name'] > newest:
+					newest = ami['Name']
+					LOGGER.debug('newest is now ' + ami['Name'])
+					amiid = ami['ImageId']
+		
+		return amiid
 
 def send_response(event, context, status, data=None):
 
 	response_url = None
 
-	if 'ResponseUrl' in event:
+	if 'ResponseURL' in event:
 		response_url = event['ResponseURL']
 	else:
-		LOGGER.warning('Request did not contain a ResponseUrl.')
-		LOGGER.info(json.dumps(data, indent=2))
+		LOGGER.warning('Request did not contain a ResponseURL.')
+		if data:
+			LOGGER.info(data)
 		return 500
 
 	response_body = {}
@@ -96,7 +105,7 @@ def send_response(event, context, status, data=None):
 	}
 
 	try:
-		response = requests.put(response_url, data=json_body, headers=headers, proxies=PROXIES)
+		response = requests.put(response_url, data=json_body, headers=headers)
 
 	except Exception as e:
 		LOGGER.error("Failed to send response. stack-id=%(stack)s, resource-type=%(rt)s, err=%(err)s, url=%(url)s" %
@@ -109,7 +118,7 @@ def send_response(event, context, status, data=None):
 		return 500
 
 	else:
-		LOG.info("Response sent, status=%(code)s, url=%(url)s" %
+		LOGGER.info("Response sent, status=%(code)s, url=%(url)s" %
 				{'code': response.reason, 'url': response_url})
 		return response.reason
 
@@ -117,36 +126,49 @@ def lambda_handler(event, context):
 
 	responseData = {}
 	status = 'FAILED'
+	
+
+	print(json.dumps(event, indent=2))
 
 	if 'RequestType' not in event:
 		event['RequestType'] = 'search'
 
-	# for update and create events run the query and return a result. For delete just return success
+	#
+	# for update run the query and return a result. For delete and create just return success
+	#
 	if event['RequestType'].lower() == 'delete':
-		send_response(event, context, SUCCESS)
+		send_response(event, context, 'SUCCESS')
 		return
+	# if event['RequestType'].lower() == 'create':
+	# 	send_response(event, context, 'SUCCESS')
+	# 	return
 
-	if 'ResourceProperties' not in event or \
-	   'Query' not in event['ResourceProperties']:
-		responseData = {'Error': 'Invalid request: missing Query'}
-	else:
+
+	if 'ResourceProperties' in event and \
+	   'Query' in event['ResourceProperties'] and 'QueryProps' in event['ResourceProperties']:
 		# Alias the dicts for simplicity
 		resProps = event['ResourceProperties']
 		queryProps = resProps['QueryProps']
 
 		if resProps['Query'] == 'GetAZforSubnet':
-			responseData = get_az_for_subnet(queryProps['subnetid'])
+			response = AWSFinder.get_az_for_subnet(queryProps['subnetid'])
+			if response:
+				responseData = { 'AZ': response }
 
 		elif resProps['Query'] == 'GetAMI':
 			if 'Architecture' not in queryProps:
 				responseData = {'Error': 'Missing QueryProps[\'Architecture\']'}
 			else:
-				responseData = get_ami(queryProps)
+				response = AWSFinder.get_ami(queryProps)
+				if response:
+					responseData = { 'AMIId': response }
 		else:
 			responseData = {'Error': 'unrecognized query ' + json.dumps(event)}
+	else: 
+		responseData = {'Error': 'Invalid request: missing Query'}
 
 	if responseData and 'Error' not in responseData:
-		status = 'Success'
+		status = 'SUCCESS'
 	else:
 		LOGGER.warning(responseData['Error'])
 
@@ -159,7 +181,5 @@ if LOGGING_LEVEL not in ('INFO', 'DEBUG', 'ERROR', 'WARNING'):
 	print('Error: LOGGING_LEVEL defaulted to DEBUG')
 
 LOGGER = configLogging(LOGGER, LOGGING_LEVEL)
-
-AWS = AWSClient()
-
 REGION = getLambdaEnv('AWS_DEFAULT_REGION')
+AWSFinder = Finder()
